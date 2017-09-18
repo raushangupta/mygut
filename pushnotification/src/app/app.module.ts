628516b206160184bf708597d4ee0c0ddb706b8f
RKG <raushan@innoventes.com>
2017-09-18 12:30:38 +0530

    successs

---
+++ pushnotification/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '180305',
+  },
+  'push': {
+    'sender_id': '647873680218',
+    'pluginConfig': {
+      'ios': {
+        'badge': true,
+        'sound': true
+      },
+      'android': {
+        'iconColor': '#343434'
+      }
+    }
+  }
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
